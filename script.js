9d21dde97b8e6eefec13709ae36ed6c1efdcfbd5
V. Karthik <36740831+karthikbilaspur@users.noreply.github.com>
2024-05-07 14:16:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const searchForm = document.querySelector('.search-form');
+const searchBtn = document.querySelector('#search-btn');
+const ProjectItem = document.querySelector('.projects-container')
+const ProjecttBtn = document.querySelector('#project-btn')
+const navbar = document.querySelector('.navbar')
+const menuBtn = document.querySelector('#menu-btn')
+
+
+
+searchBtn.addEventListener('click', ()=>{
+    searchForm.classList.toggle('active');
+    document.addEventListener('click', (e)=>{
+        if(!e.composedPath().includes(searchBtn) && !e.composedPath().includes(searchForm) ){
+            searchForm.classList.remove('active');
+        }
+    })
+})
+
+ProjecttBtn.addEventListener('click', ()=>{
+    cartItem.classList.toggle('active');
+    document.addEventListener('click', (e)=>{
+        if(!e.composedPath().includes(cartBtn) && !e.composedPath().includes(cartItem) ){
+            cartItem.classList.remove('active');
+        }
+    })
+})
+
+menuBtn.addEventListener('click', ()=>{
+    navbar.classList.toggle('active');
+    document.addEventListener('click', (e)=>{
+        if(!e.composedPath().includes(navbar) && !e.composedPath().includes(menuBtn) ){
+            navbar.classList.remove('active');
+        }
+    })
+})
